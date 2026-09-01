7c1bfa09ec0456a805f1ea3b36ca891e6fc2a2eb
valux0 <valerihercruz@gmail.com>
2026-09-01 16:37:52 +0000

    jelou karin

--- hello-dash.py
+++ hello-dash.py
@@ -1,6 +1,7 @@
 import streamlit as st
-import pandas as pd 
-st.title("hello streamlit")
+import pandas as pd
+st.title("hello page")
+st.write("karin es bello")
 dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
 st.dataframe(dataframe)
-st.write("by adsoftsito")
+
